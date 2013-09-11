ba733da15b2c486b5133f77fb21c12c0bbcfaa08
= <=>
2013-09-11 10:26:57 +0900

    imply uiconfig

--- Resources/uiconfig.js
+++ Resources/uiconfig.js
@@ -1,7 +1,19 @@
 /*
  * created by fvi@
  * 
+ * これはイメージのパスの配置を記述するための設定ファイルです
+ * うｐ主は眠くて寝てしまいました。あとはやっておいて上げてください
  * 
- * 
- * 
+ * パスはResources/　から記述するようにしてください
  */
+
+/*
+ * 共通フレームの上のリボンイメージ
+ */
+exports.COMMON_UP_BAR_IMAGE_PATH = '/images/underRibbon/underRibbon2.png';
+
+/*
+ * オプションビューの遷移アニメーションの所要時間（ミリ秒)
+ */
+exports.OPT_VIEW_MOVE_ANIMATION_OPEN_DURING = 700;
+exports.OPT_VIEW_MOVE_ANIMATION_CLOSE_DURING = 700;
