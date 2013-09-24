89bf92d5fbd563109b2cdcb0590cf95374a21938
ikounakis <johnrhogr@gmail.com>
2013-09-24 20:35:30 +0900

    * Added header and footer * Fixed the length of the tab label * Added a couple more stub sns buttons

--- Resources/confirmWindow.js
+++ Resources/confirmWindow.js
@@ -4,17 +4,50 @@
  */
 
 exports.createConfrimWindow = function() {
-	
+	var uiconfig = require('uiconfig');
+	 	
 	var baseWindow = Titanium.UI.createWindow({
 		backgroundColor: 'white',
 		fullscreen: true,
 		orientationModes: [Titanium.UI.PORTRAIT]
 	});
-	
-	var mainView = Ti.UI.createView({
-		backgroundColor: 'white'
-	});
 
+	/*
+	 * Create the header and add the label only
+	 */
+	var commonParts = require('CreateCommonParts');
+	
+	var upperRibbon = commonParts.createUpperRibbon();
+	baseWindow.add(upperRibbon);
+
+	var titleLabel = commonParts.createTitleLabel();
+	baseWindow.add(titleLabel);
+	
+	/*
+	 * Create the footer and the associated buttons
+	 */
+	var underRibbon = commonParts.createUnderRibbon();
+	baseWindow.add(underRibbon);
+		
+	var homeButton = commonParts.createHomeButton();
+	baseWindow.add(homeButton);
+	
+	var listButton = commonParts.createListButton();
+	baseWindow.add(listButton);	
+	
+	var peopleButton = commonParts.createPeopleButton();
+	baseWindow.add(peopleButton);
+	
+	/*
+	 * Create main view
+	 */
+	var ribbonsHeight = uiconfig.HEADER_RIBBON_HEIGHT;
+	var mainView = Ti.UI.createView({
+		backgroundColor: 'white',
+		top: ribbonsHeight,
+		bottom: ribbonsHeight
+	});
+	
 	baseWindow.add(mainView);
 	
 	var mangaWebView = Ti.UI.createWebView({
@@ -27,22 +60,25 @@ exports.createConfrimWindow = function() {
 	
 	mainView.add(mangaWebView);
 	
+	var originalTagLabel = 'Tags: ';
 	var tagLabel = Ti.UI.createLabel({
 		color: 'black',
 		font: { fontSize: '14dp' },
-		text: 'Tags: ',
+		text: originalTagLabel,
 		top: '55%',
-		left: '5%'
+		left: '5%',
+		width: '90%'
 	});
 	
 	mainView.add(tagLabel);
 	
 	var tagTextField = Ti.UI.createTextField({
 		borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+		hintText: 'Enter related tags',
 		color: 'black',
 		top: '60%',
 		left: '5%',
-		width: '45%',
+		width: '65%',
 		
 	});
 	
@@ -51,18 +87,32 @@ exports.createConfrimWindow = function() {
 	var tagButton = Ti.UI.createButton({
 		title: 'Add Tag',
 		top: '60%',
-		left: '55%'
+		left: '70%',
+		width: '25%'
 	});
 	
 	tagButton.addEventListener('click', function() {
 		var newTagString = tagTextField.getValue();
 		var oldTagString = tagLabel.getText();
+		var combinedTagString;
 		
 		if (newTagString.length > 0) {
-			if (oldTagString.length > 6) {							// 6 is the size of the original text of the label -> 'Tags: '.
-				tagLabel.setText(oldTagString + ', ' + newTagString);
+			if (oldTagString.length > originalTagLabel.length) {
+				combinedTagString = oldTagString + ', ' + newTagString;
+				if (combinedTagString.length > 40) {
+					alert('No more tags available');
+					return;
+				} else {
+					tagLabel.setText(combinedTagString);
+				}
 			} else {
-				tagLabel.setText(oldTagString + newTagString);		
+				combinedTagString = oldTagString + newTagString;
+				if (combinedTagString.length > 40) {
+					alert('Please enter a shorter tag');
+					return;
+				} else {
+					tagLabel.setText(combinedTagString);
+				}	
 			}
 		}
 		
@@ -74,15 +124,35 @@ exports.createConfrimWindow = function() {
 	var facebookButton = Ti.UI.createButton({
 		title: 'Facebook',
 		top: '70%',
-		left: '5%'
+		left: '5%',
+		width: '25%'
 	});
 	
 	mainView.add(facebookButton);
 	
+	var twitterButton = Ti.UI.createButton({
+		title: 'Twitter',
+		top: '70%',
+		left: '30%',
+		width: '25%'
+	});
+	
+	mainView.add(twitterButton);
+	
+	var pinterestButton = Ti.UI.createButton({
+		title: 'Pinterest',
+		top: '70%',
+		left: '55%',
+		width: '25%'
+	});
+	
+	mainView.add(pinterestButton);
+	
 	var postButton = Ti.UI.createButton({
 		title: 'Post',
 		top: '70%',
-		left: '30%'		
+		left: '80%',
+		width: '15%'
 	});
 	
 	mainView.add(postButton);
